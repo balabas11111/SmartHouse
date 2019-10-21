a4fbb5bbd787e1f1f4bec2dc4ef68b3af5583ed6
Vitaliy_vlasov <Vitaliy.Vlasov@thomsonreuters.com>
2019-10-21 19:31:57 +0300

    clean up

--- projects/Arduino_miniPro_movement_sensor/Release/core/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-core\core\abi.cpp.o: \
- D:\projects\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino\abi.cpp
--- projects/Arduino_miniPro_movement_sensor/Release/core/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-core\core\hooks.c.o: \
- D:\projects\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.11\cores\arduino\hooks.c
