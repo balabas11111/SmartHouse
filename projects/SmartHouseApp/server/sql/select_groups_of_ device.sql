d018a5dd9b28542676845ece5d59675174d58126
Vitaliy_vlasov <Vitaliy.Vlasov@thomsonreuters.com>
2019-10-24 14:17:05 +0300

    script updates

--- projects/SmartHouseApp/server/sql/select_groups_of_ device.sql
+++ projects/SmartHouseApp/server/sql/select_groups_of_ device.sql
@@ -1,12 +1,14 @@
+SELECT* FROM devices;
+
 SELECT g.* FROM devices d
 	JOIN groups g ON g.device_id = d.id
-WHERE d.id = 1;
+WHERE d.id = 3;
 
 SELECT e.*, ef.* FROM entities e
 JOIN entity_field ef ON ef.entity_id = e.id
-WHERE e.group_id = 2;
+WHERE e.group_id = 8;
 
 SELECT e.name, ef.description, ef.name, efev.* FROM entities e
 JOIN entity_field ef ON ef.entity_id = e.id
 JOIN entity_field_enabled_value efev ON efev.entity_field_id = ef.id
-WHERE e.group_id = 2;
+WHERE e.group_id = 8;
