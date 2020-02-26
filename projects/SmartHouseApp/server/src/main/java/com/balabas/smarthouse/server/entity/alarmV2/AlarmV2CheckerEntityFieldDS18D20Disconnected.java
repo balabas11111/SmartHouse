175cabf2b4d9e0cc5cd7b900e6b840fdafb96015
Vitaliy_vlasov <Vitaliy.Vlasov@thomsonreuters.com>
2020-02-26 18:17:44 +0200

    checker refactored

--- projects/SmartHouseApp/server/src/main/java/com/balabas/smarthouse/server/entity/alarmV2/AlarmV2CheckerEntityFieldDS18D20Disconnected.java
+++ projects/SmartHouseApp/server/src/main/java/com/balabas/smarthouse/server/entity/alarmV2/AlarmV2CheckerEntityFieldDS18D20Disconnected.java
@@ -2,11 +2,8 @@ package com.balabas.smarthouse.server.entity.alarmV2;
 
 import org.springframework.stereotype.Component;
 
-import com.balabas.smarthouse.server.entity.model.entityfields.IEntityField;
-
 import lombok.Getter;
 
-@SuppressWarnings("rawtypes")
 @Component
 public class AlarmV2CheckerEntityFieldDS18D20Disconnected extends AlarmV2CheckerAbstractEntityFieldNumber implements AlarmV2Checker {
 
@@ -15,10 +12,9 @@ public class AlarmV2CheckerEntityFieldDS18D20Disconnected extends AlarmV2Checker
 	
 	@Override
 	protected boolean checkItemValue(IAlarmV2 alarm) {
-		IEntityField item = (IEntityField) alarm.getItem();
-		int val = getValueTmpAsInteger((IEntityField) item).intValue();
+		int tmp = getValueTmpAsInteger(getItemAsEntityField(alarm)).intValue();
 		
-		return !(val == -127 || val == 85);
+		return !(tmp == -127 || tmp == 85);
 	}
 	
 	@Override
