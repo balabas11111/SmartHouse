161424b3b22ec24fd3edcccf122026b57d1e7288
Vitaliy_vlasov <Vitaliy.Vlasov@thomsonreuters.com>
2020-03-30 09:40:43 +0300

    commands

---
+++ projects/SmartHouseApp/server/bin/start.sh
@@ -0,0 +1,9 @@
+process=`ps -ef | grep -v awk | awk -e '/sudo.*java.*server-2.2.0.M6.jar/ { print $2 }'`
+
+if [ -z "$process" ]
+then
+      echo "\smartHouseServer has not started as sudo"
+      sudo java -jar /home/pi/apps/SmartHouseApp/server-2.2.0.M6.jar
+else
+      echo "\pid = $process smartHouseServer already runs"
+fi
